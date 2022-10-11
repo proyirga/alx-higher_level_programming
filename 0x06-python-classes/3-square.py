78578fcd91e568a4a7a66d040a9128707c50189f
Yirga Mulaw <infoyirga12@gmail.com>
2022-10-11 22:11:34 +0300

    3. Area of a square

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -3,24 +3,23 @@
 
 
 class Square:
-    """Instantiation with optional size"""
-    
+    """Initializing this class"""
 
-    def __init__(self, size = 0):
-        """Validate size is an integer
-        and not less than 0"""
-    
+    def __init__(self, size=0):
+        """Validating size attribute is an
+        integer and not less than zero
+        """
 
         if not isinstance(size, int):
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
-        
-        self.__size =  size
 
+        self.__size = size
 
     def area(self):
-        """Returns area"""
+        """
+        Calculate area of the square
+        """
 
-
-        return (self.__size * self.__size)
+        return (self.__size ** self.__size)
