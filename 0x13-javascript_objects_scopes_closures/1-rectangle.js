4bfe603728321f089406fa85d4bc410f1af066fe
Yirga Mulaw <infoyirga12@gmail.com>
2022-12-20 21:36:54 +0300

    trailing space

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 /**
  * A class Rectangle that defines a rectangle:
- * 
+ *
  * ***You must use the class notation for defining your class
  * ***The constructor must take 2 arguments w and h
  * ***Initialize the instance attribute width with the value of w
