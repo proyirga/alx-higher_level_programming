61d80fdd79050eb7a424004656389f07bd5c9954
Yirga Mulaw <infoyirga12@gmail.com>
2022-12-20 21:44:44 +0300

    Tab

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 /**
  * A class Square that defines a square and inherits from Square
- * 	***of 5-square.js:
+ * ***of 5-square.js:
  *
  * ***You must use the class notation for defining your class and extends
  * ***Create an instance method called charPrint(c) that prints the rectangle
- * 	***using the character c
+ * ***using the character c
  * ***If c is undefined, use the character X
  *
  */
