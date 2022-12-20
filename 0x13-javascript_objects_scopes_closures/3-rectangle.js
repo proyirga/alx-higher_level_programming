8c7fa0e3d54dd86b566db24dc9452ade893e96a2
Yirga Mulaw <infoyirga12@gmail.com>
2022-12-20 21:40:45 +0300

    Tab

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -8,7 +8,7 @@
  * ***Initialize the instance attribute height with the value of h
  * ***If w or h is equal to 0 or not a positive integer, create an empty object
  * ***Create an instance method called print() that prints the rectangle using
- * 	***the character X
+ * ***the character X
  */
 class Rectangle {
   constructor (w, h) {
