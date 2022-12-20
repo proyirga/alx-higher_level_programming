1d74a0091dda0daf07f6807e1f1a11c5b5bcc033
Yirga Mulaw <infoyirga12@gmail.com>
2022-12-20 21:43:39 +0300

    Tab

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 /**
  * A class Square that defines a square and inherits from Rectangle
- * 	***of 4-rectangle.js:
+ * ***of 4-rectangle.js:
  * ***You must use the class notation for defining your class and extends
  * ***The constructor must take 1 argument: size
  * ***The constructor of Rectangle must be called (by using super())
