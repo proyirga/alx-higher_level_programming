fcc77e40ce91c7df5b5adcc69754a598bd9b8dc0
Yirga Mulaw <infoyirga12@gmail.com>
2023-07-25 19:03:04 +0300

    Load json from file

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Write a function that creates an Object
+from a “JSON file”:"""
+import json
+
+
+def load_from_json_file(filename):
+    """load from json file"""
+    with open(filename) as f:
+        return (json.load(f))
