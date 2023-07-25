1188425a3b7c3cc3277823821f5b3eb3bceb47fb
Yirga Mulaw <infoyirga12@gmail.com>
2023-07-25 17:24:50 +0300

    read file to STDIO

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,6 +4,6 @@ def read_file(filename=""):
     """Reads a file filename
     Args: (filename)"""
 
-    with open("filename", encoding="UTF-8") as f:
+    with open(filename, encoding="UTF-8") as f:
         for line in f:
             print(line)
