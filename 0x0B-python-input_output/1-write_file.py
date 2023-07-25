23abf79f8d52af586b67c4aa428c8467752925da
Yirga Mulaw <infoyirga12@gmail.com>
2023-07-25 18:06:53 +0300

    Write to file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,7 +5,11 @@ and returns the number of characters written:"""
 
 def write_file(filename="", text=""):
     """write a string to a text file
-    Args: (filename) (text)""" 
+    Args: 
+        filename: name of the file
+        text: strings to be written to a file filename
+    """ 
 
     with open(filename, "w", encoding="UTF-8") as f:
-        return (f.write(text))
+        write_date = f.write(text)
+        return (write_date)
