adbb18b6c9cc3d43b8095b6fdb3f5eb7b1ab98ca
Yirga Mulaw <infoyirga12@gmail.com>
2023-09-13 10:23:54 +0300

    Pycodestyle validation correction

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Write a function that returns the JSON representation of an object (string):"""
+"""
+Write a function that returns the JSON representation of
+an object (string):
+"""
 import json
 
 
