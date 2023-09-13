10a77c1de99086b07294057784246e5d340289b7
Yirga Mulaw <infoyirga12@gmail.com>
2023-09-13 10:35:37 +0300

    From JSON to String

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/python3
+"""
+Function that returns an obj represented by JSON
+"""
+
+import json
+
+
+def from_json_string(my_str):
+    """Return string rep of json"""
+    return json.load(my_str)
